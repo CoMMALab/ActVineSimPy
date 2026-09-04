c924b583f32aa8749b5207aa887b0ab019353bc5
Your Name <you@example.com>
2026-09-04 13:33:11 -0400

    Was going to move main loop, but decided to not do that

--- xnew_rrt/rrt_util.py
+++ xnew_rrt/rrt_util.py
@@ -23,7 +23,6 @@ Questions:
           Above kind of changes how the whole structure operates...
 '''
 
-
 #--------------------------------------------------------- Data Structures
 
 class StateInfo:
@@ -97,22 +96,24 @@ class RRTTree:
     ''' 
 
     def __init__(self, start_state, distance_function, goal_test,
-                 rest_of_goal_test_args,
-                 rest_of_distance_func_args):
+                 aux_goal_test_args,
+                 aux_distance_func_args,
+                 num_closest_paths=0):
+        
         self.root = Node(start_state)
         self.distance_function = distance_function
         self.goal_test = goal_test
 
         # Packed args for rest of goal test args
-        # self.vine_radius = vine_radius
-        # self.goal_coords = goal_coords
-        # self.goal_radius = goal_radius
-        self.goal_test_args = rest_of_goal_test_args
+        self.goal_test_args = aux_goal_test_args
 
         # Packed args for rest of distance func args
-        # self.max_bodies = max_bodies
-        # self.num_moveable_objs = num_moveable_objs
-        self.distance_func_args = rest_of_distance_func_args
+        self.distance_func_args = aux_distance_func_args
+
+        # When no path in the tree finds a goal state,
+        # the tree will return "num_closest_paths"-many paths
+        # that were closest to the goal region
+        self.num_closest_paths = num_closest_paths
 
     def find_nearest_to(self, new_state: StateInfo):
 
@@ -162,112 +163,3 @@ class RRTTree:
         
         else:
             return None
-
-            
-#----------------------------------------------------------------------- RRT Distance Funcs
-
-def euclidean_distance(state_obj1: StateInfo, state_obj2: StateInfo, 
-                 max_bodies, num_moveable_objs,
-                 theta_weight = 1, 
-                 weight_list = (1, 1, 1, 1, 1)):
-    '''
-    Default distance function for comparing states returned by sim
-    On args:
-        - theta_weight: how much to weight diff in theta compared to diff in position
-        - weight_list: how much to multiply each measure by (arbitrarily decided)
-
-    NOTE: everything stays in non-dim units, but I don't think that really matters... :p
-    '''
-
-    def vmapped_distance(pose1, pose2, theta_weight):
-        # Tentative measure for angle diff: 
-        # dtheta = torch.atan2(torch.sin(theta1 - theta2), torch.cos(theta1 - theta2))
-
-        # Euclidean distance is used otherwise
-
-        x1, y1, theta1 = pose1
-        x2, y2, theta2 = pose2
-
-        euclid_dist = torch.sqrt((x2 - x1)**2 + (y2 - y1)**2)
-        dtheta = torch.atan2(torch.sin(theta2 - theta1), torch.cos(theta2 - theta1))
-
-        return euclid_dist + theta_weight * dtheta
-
-    #NOTE: reshapes are to make things easier for vmap
-
-    vine_state1 = state_obj1["state"].reshape(B * max_bodies, 3)
-    vine_state2 = state_obj2["state"].reshape(B * max_bodies, 3)
-
-    vine_dstate1 = state_obj1["dstate"].reshape(B * max_bodies, 3)
-    vine_dstate2 = state_obj2["dstate"].reshape(B * max_bodies, 3)
-
-    bodies1 = state_obj1["bodies"].reshape(B * 1)
-    bodies2 = state_obj2["bodies"].reshape(B * 1)
-
-    obj_state1 = state_obj1["moveable_obj_pose"].reshape(B * num_moveable_objs, 3)
-    obj_state2 = state_obj2["moveable_obj_pose"].reshape(B * num_moveable_objs, 3)
-
-    obj_dstate1 = state_obj1["moveable_obj_dstate"].reshape(B * num_moveable_objs, 3)
-    obj_dstate2 = state_obj2["moveable_obj_dstate"].reshape(B * num_moveable_objs, 3)
-
-    # Diff for vine states:
-    vine_state_diff = torch.vmap(vmapped_distance, in_dims=(0, 0, None))(vine_state1, vine_state2, theta_weight)
-    vine_state_diff = torch.sum(vine_state_diff)
-
-    # Diff for vine dstates:
-    vine_dstate_diff = torch.vmap(vmapped_distance, in_dims=(0, 0, None))(vine_dstate1, vine_dstate2, theta_weight)
-    vine_dstate_diff = torch.sum(vine_dstate_diff)
-
-    # Diff for bodies:
-    bodies_diff = torch.abs(bodies2 - bodies1)
-
-    # Diff for moveable object states:
-    obj_state_diff = torch.vmap(vmapped_distance, in_dims=(0, 0, None))(obj_state1, obj_state2, theta_weight)
-    obj_state_diff = torch.sum(obj_state_diff)
-
-    # Diff for moveable object dstates:
-    obj_dstate_diff = torch.vmap(vmapped_distance, in_dims=(0, 0, None))(obj_dstate1, obj_dstate2, theta_weight)
-    obj_dstate_diff = torch.sum(obj_dstate_diff)
-
-    # Return one weighted measure:
-
-    state_w, dstate_w, bodies_w, obj_state_w, obj_dstate_w = weight_list
-
-    return (vine_state_diff * state_w) + \
-           (vine_dstate_diff * dstate_w) + \
-           (bodies_diff * bodies_w) + \
-           (obj_state_diff * obj_state_w) + \
-           (obj_dstate_diff * obj_dstate_w)
-
-
-
-#----------------------------------------------------------------------- RRT Goal Tests
-
-def last_body_goal_test(state_obj: StateInfo, 
-                        max_bodies,
-                        vine_radius, # in m, for consistency
-                        goal_coords, # (x, y) given in m
-                        goal_radius  # given in m
-                        ):
-    '''
-    Simple goal test: if the last body is anywhere within the 
-    goal region, then returns True (otherwise returns False).
-
-    NOTE: assumes both the vine body's and the goal region's geometries
-          are simple circles.
-    '''
-
-    num_bodies = state_obj["bodies"].reshape(B * 1)
-
-    last_body_x, last_body_y, last_body_theta = state_obj["state"].reshape(B * max_bodies, 3)[num_bodies - 1].squeeze()
-
-    # Convert internal dims to meters:
-    last_body_x = MM(last_body_x) / 1000; last_body_y = MM(last_body_y) / 1000
-
-    min_dist_before_collision = goal_radius + vine_radius
-
-    distance = torch.sqrt((goal_coords[0] - last_body_x)**2 + (goal_coords[1] - last_body_y)**2)
-
-    return distance < min_dist_before_collision
-
-
